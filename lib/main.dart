12b3a30ada31a4da27d5b61715ea7d50dfa84d98
Omar Elmanzalawy <90012228+OmarElmanzalawy@users.noreply.github.com>
2024-11-08 10:22:37 +0200

    firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone2/firebase_options.dart';
 import 'package:tiktok_clone2/screens/auth/register_screen.dart';
 
 void main() async{
